b4a6ce6a0ab9e5d8e6a9cf5258f3598cee69d309
ayushmavl <149374395+ayushmavl@users.noreply.github.com>
2023-10-30 23:45:34 +0530

    functionarea.c

---
+++ functionarea.c
@@ -0,0 +1,24 @@
+-#include<stdio.h>
+
+void square(int n);
+void circle(int m);
+
+
+int main()
+{
+    int n=2;
+    float m=5.;
+    printtf("%d\n",square(int n);
+   printf("%d" circle(int m);
+    return 0;
+}
+ void square(int n)
+ {
+ return n*n;
+ }
+ void circle(int m)
+ {
+     m=(3.14*m)*m;
+     printf("the area circle=%f",m);
+ }
+ 
