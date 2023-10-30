f352efad5eced5104a2760dded98b9545a0e232a
ayushmavl <149374395+ayushmavl@users.noreply.github.com>
2023-10-30 15:21:52 +0530

    function.c

---
+++ function.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void ayush();
+
+int main() {
+   int n;
+    scanf("%d",&n);
+   ayush();
+    return 0;
+}
+void ayush()
+{
+    int n=1;
+    n=n+1;
+    printf("lode%d",n);
+    
+}
